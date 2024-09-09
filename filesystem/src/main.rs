92c0fb20c0774b25590391b946f4e7f1219d7fcc
Callahan <callahancarvalho@gmail.com>
2024-09-09 17:03:55 +0530

    done

--- filesystem/src/main.rs
+++ filesystem/src/main.rs
@@ -4,14 +4,15 @@ use dirs::{config_dir, home_dir};
 
 struct ConfigOptions{
     filesystem_location:PathBuf,
+    filesystem_size:u64
 }
 
 fn main() -> std::io::Result<()> {
     let config_dir = config_dir().unwrap(); 
     let config_path: PathBuf = [config_dir.to_str().unwrap(),"walrust.conf"].iter().collect();
+    println!("{:?}",config_path);
     let mut config_file = OpenOptions::new()
         .read(true)
-        .write(true)
         .create(true)
         .open(config_path);
    
