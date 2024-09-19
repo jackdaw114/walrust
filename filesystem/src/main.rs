c1848968c930da80ba288f80a6d922021b914871
Callahan <callahancarvalho@gmail.com>
2024-09-19 19:31:41 +0530

    conf file reading and assigning to a struct

--- filesystem/src/main.rs
+++ filesystem/src/main.rs
@@ -20,3 +20,61 @@ fn main() -> std::io::Result<()> {
     Ok(())
      
 }
+
+// To be included
+// use std::{collections::HashMap, fs::File, io::Read};
+
+// fn main() {
+//     let mut file = File::open("conf_Test.txt").unwrap();
+//     let mut data = String::new();
+//     file.read_to_string(&mut data).unwrap();
+
+//     // #[derive(Debug)]
+//     struct NodeConfiguration {
+//         f_size: u128,
+//         author: String,
+//         f_data: String
+//     }
+
+//     impl NodeConfiguration {
+//         fn obtain_size(&mut self, string:&str) {
+//             self.f_size = string[..string.len()-2].parse().unwrap(); //consider using .expect("unexpected size defined")
+//         }
+
+//         fn display_node(&self){
+//             println!("author:{}\ndefined size:{}\ndata:{}", self.author, self.f_size, self.f_data)
+//         }
+//     }
+
+
+//     let mut conf_dict: HashMap<&str, &str> = HashMap::new();
+//     let mut skip_flag: bool = false;
+//     for string in data.split_whitespace() {
+//         //println!("{:?}", &string);
+//         if string == "*/"{
+//             skip_flag = false;
+//             continue;
+//         }
+        
+//         if string == "/*"{
+//             skip_flag = true;
+//         }
+//         if skip_flag {
+//             continue;
+//         }
+//         conf_dict.insert(&string[0..6], &string[7..]);
+//     }
+
+//     //println!("{:?}", conf_dict);
+
+//     let mut new_node = NodeConfiguration{
+//         f_size : 0,
+//         author : conf_dict["author"].to_string(),
+//         f_data : conf_dict["f_data"].to_string()
+//     };
+
+//     new_node.obtain_size(conf_dict["f_size"]);
+//     new_node.display_node();
+//     //println!("{:?}", new_node);
+
+// }
