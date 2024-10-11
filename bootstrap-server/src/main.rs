ce9ef223fb84524f68d4c92f9ef8e542354fa810
jackdaw114 <80084582+jackdaw114@users.noreply.github.com>
2024-10-11 19:07:41 +0530

    small

--- bootstrap-server/src/main.rs
+++ bootstrap-server/src/main.rs
@@ -73,7 +73,7 @@ async fn add_to_network(
     
     let route_map = routing_table.lock().unwrap();
     for (hash,entry) in route_map.iter(){
-        if hash = 
+        // TODO: Check hash params here and then enter it into file
     }
     format!("Hello, {}! Your IP address is: {}", port, addr.ip())
 }
@@ -115,50 +115,3 @@ fn loadRoutingTable(filename: &PathBuf) -> io::Result<HashMap<String, RouteEntry
 
     Ok(hashmap)
 }
-/*
-fn main() -> std::io::Result<()> {
-    let finch_dir = "/tmp/finch";
-    if let Err(x) = create_dir_all(finch_dir){
-        return Err(x);
-    }
-    let stdout = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .open(finch_dir.to_string() + "/finch-bootstrap_server.out")
-        .unwrap();
-
-
-    let stderr = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .open(finch_dir.to_string() + "/finch-bootstrap_server.err")
-        .unwrap();
-   
-
-    let daemon = Daemonize::new()
-        .pid_file("/tmp/test.pid") 
-        .chown_pid_file(true)      
-        .working_directory("/tmp") 
-        .user("nobody")
-        .group("daemon") //TODO: create a custom group with privilidges as well as a
-                                       //custom user
-        .group(2)        
-        .umask(0o777)    
-        .stdout(stdout)  
-        .stderr(stderr)  
-        .privileged_action(|| "Executed before drop privileges"); 
-
-
-    match daemon.start(){
-        Ok(_) => {
-            println!("inside daemon");
-
-        }
-        Err(e) =>{
-            eprintln!("Error, {}",e);
-        }
-    }
-
-    Ok(())
-}
-*/
